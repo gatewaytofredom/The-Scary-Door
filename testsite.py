2821ea03bf05a0c857ecf84e732d866758e6870b
gatewaytofredom <bobdosgames@gmail.com>
2017-12-12 22:56:03 -0600

    working code?

--- testsite.py
+++ testsite.py
@@ -7,33 +7,33 @@ GPIO.setmode(GPIO.BCM)
 
 @app.route("/")
 def hello():
-    templateData = {
+  templateData = {
 
-      }
-    return render_template('main.html', **templateData)
+    }
+  return render_template('main.html', **templateData)
 
 @app.route("/readPin/")
 def readPin():
-    try:
-      GPIO.setup(int(23), GPIO.IN)
+  try:
+    GPIO.setup(int(23), GPIO.IN)
     if GPIO.input(int(23)) == True:
-         response = randint(0,10)
-		if response <= 5:
-			output="<font color="red">Nice!</font>"
-		else
-			output="<font color="red">Naughty!</font>"
+      response = randint(0,10)
+      if response <= 5:
+		    output="Nice"
+      else:
+		    output="Naughty"
     else:
-         response = "not pressed"
-			except:
-		response = "There was an error reading pin " + 23 + "."
+      response = "not pressed"
+  except:
+	response = "There was an error reading pin " + 23 + "."
 
-    templateData = {
-      'title' : 'Status of Pin' + 23,
-      'response' : response
-	  'screenout' : output
-      }
+  templateData = {
+    'title' : 'Status of Pin' + 23,
+    'response' : response,
+    'screenout' : output
+  }
 
-    return json.dumps(templateData)
+	return json.dumps(templateData)
 
 
 if __name__ == "__main__":
